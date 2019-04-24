eed4871cf5c23c92d51a79e300a8838638d5aab4
Bogusz <bognapierala@gmail.com>
2019-04-24 19:29:53 +0200

    Listening on port

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const config = require("./setup/config");
-const db = require("./setup/db");
+const express = require("express");
+const logger  = require("./utils/logger");
 
+const config  = require("./setup/config");
+const db      = require("./setup/db");
+const routes  = require("./setup/routes");
+
+const app = express();
 
 config();
 db();
+routes(app);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => logger.info(`Listening on port ${port}...`))
