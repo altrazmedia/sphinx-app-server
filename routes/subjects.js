607af71b024c3f87911cc9c0ca02faa79f3d432d
Bogusz <bognapierala@gmail.com>
2019-04-27 14:29:55 +0200

    Basic Subject requests

---
+++ routes/subjects.js
@@ -0,0 +1,108 @@
+const express  = require("express");
+const mongoose = require("mongoose");
+
+const { Subject }     = require("../models/Subject");
+const asyncMiddleware = require("../middleware/asyncMiddleware");
+
+const router = express.Router();
+
+/**
+ * Getting the list of subjects
+ */
+router.get("/", asyncMiddleware(async (req, res) => {
+  const subjects = await Subject.find().sort("name")
+  res.send(subjects);
+}));
+
+
+/**
+ * Creating a new subject
+ */
+router.post("/", asyncMiddleware(async (req, res) => {
+  const { name, code } = req.body;
+
+  const missingFields = [];
+  if (typeof name !== "string") { missingFields.push("name"); }
+  if (typeof code !== "string") { missingFields.push("code"); }
+
+  if (missingFields.length > 0) {
+    // Required fields are missing
+    return res.status(400).send({ required: missingFields })
+  }
+
+  const matchingSubjects = await Subject.find({ code });
+  if (matchingSubjects.length > 0) {
+    // There already is a subject with given code
+    return res.status(409).send({ duplicate: [ "code" ] })
+  }
+  
+  let subject = new Subject({ code, name });
+  subject = await subject.save();
+
+  return res.send(subject);
+
+}));
+
+
+/**
+ * Getting the subject by id
+ */
+router.get("/:id", asyncMiddleware(async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).send({ notfound: [ "subject" ] });
+  }
+
+  const subject = await Subject.findById(id);
+  if (!subject) {
+    return res.status(404).send({ notfound: [ "subject" ] });
+  }
+
+  return res.send(subject);
+
+}));
+
+/**
+ * Editing the subject
+ */
+router.put("/:id", asyncMiddleware(async (req, res) => {
+  const { id } = req.params;
+  const { name, code } = req.body;
+
+  const uncorrectTypes = [];
+  if (name && typeof name !== "string") { uncorrectTypes.push("name"); }
+  if (code && typeof code !== "string") { uncorrectTypes.push("code"); }
+  if (uncorrectTypes.length > 0) {
+    return res.status(400).send({ type: uncorrectTypes })
+  }
+
+  if (code) {
+    const subjectByCode = await Subject.findOne({ code });
+    if (subjectByCode && String(subjectByCode._id) !== id) {
+      // There already is another subject with that code
+      return res.status(409).send({ duplicate: [ "code" ] })
+    }
+  }
+  
+
+  if (!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).send({ notfound: [ "subject" ] });
+  }
+
+  let subject = await Subject.findById(id);
+  if (!subject) {
+    return res.status(404).send({ notfound: [ "subject" ] });
+  }
+
+  subject.name = name || subject.name;
+  subject.code = code || subject.code;
+
+  subject = await subject.save();
+
+  return res.send(subject);
+
+
+}));
+
+module.exports = router;
--- setup/routes.js
+++ setup/routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
+const error = require("../middleware/error");
+const subjects = require("../routes/subjects");
+
 module.exports = function(app) {
   app.use(express.json());
 
-  
+  app.use("/api/subjects", subjects);
+
+  app.use(error); // Error middleware
 }
