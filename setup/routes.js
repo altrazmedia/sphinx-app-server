4f007a2042aeb31eeaeeb3413ef907bb19c1c1db
Bogusz <bognapierala@gmail.com>
2019-04-27 22:19:37 +0200

    Applying auth middleware to subjects route

--- setup/routes.js
+++ setup/routes.js
@@ -1,12 +1,18 @@
 const express = require("express");
 
 const error = require("../middleware/error");
+const auth  = require("../middleware/auth");
+
 const subjects = require("../routes/subjects");
+const session  = require("../routes/session");
 
 module.exports = function(app) {
   app.use(express.json());
-
+  
+  app.use("/api/subjects", auth);
   app.use("/api/subjects", subjects);
+
+  app.use("/api/session", session);
 
   app.use(error); // Error middleware
 }
