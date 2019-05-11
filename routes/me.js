682b526f8baafed2a405842c6c694a92c1b9580d
Bogusz <bognapierala@gmail.com>
2019-05-11 22:39:23 +0200

    Returning only user data on "/me" endpoint

--- routes/me.js
+++ routes/me.js
@@ -1,9 +1,6 @@
 const express  = require("express");
 const router = express.Router();
 
-const { Classes } = require("../models/Classes");
-const { Group }   = require("../models/Group");
-
 
 const asyncMiddleware = require("../middleware/asyncMiddleware");
 
@@ -13,32 +10,6 @@ const asyncMiddleware = require("../middleware/asyncMiddleware");
 router.get("/", asyncMiddleware(async (req, res) => {
 
   const user = { ...req.body.__user._doc };
-
-  if (user.role === "teacher") {
-    // fetching classes this teacher is leading
-    const classesLead = await Classes
-      .find({ teacher: user._id })
-      .populate("subject", "code name")
-      .populate("group", "code name")
-      .sort({ active: "desc" })
-    
-    user.classes_lead = classesLead; 
-  }
-
-  if (user.role ===  "student") {
-    // fetching classes the user is a part of
-    const groups = await Group
-      .find({ students: user._id });
-    
-    
-    const classes = await Classes
-      .find({ group: { $in: groups.map(group => group._id) }})
-      .populate("teacher", "label")
-      .populate("subject", "name code")
-      .sort({ active: "desc" });
-    
-    user.my_classes = classes;
-  }
 
   res.send(user);
 
