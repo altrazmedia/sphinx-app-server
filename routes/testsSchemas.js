27a32f670444a3799c594bb51024822c43c4ba97
Bogusz <bognapierala@gmail.com>
2019-06-06 22:54:22 +0200

    Gettings stats about tests using particular schema

--- routes/testsSchemas.js
+++ routes/testsSchemas.js
@@ -4,8 +4,11 @@ const mongoose = require("mongoose");
 const { Question }   = require("../models/Question");
 const { TestSchema }    = require("../models/TestSchema");
 const { Subject } = require("../models/Subject");
-const { User }    = require("../models/User");
+const { Test }    = require("../models/Test");
 const errors      = require("../utils/errorResponses");
+
+const { getTestAttempts } = require("./_utils");
+
 
 const asyncMiddleware = require("../middleware/asyncMiddleware");
 const roles           = require("../middleware/roles");
@@ -97,21 +100,70 @@ router.get("/:id", roles([ "teacher", "admin" ]), asyncMiddleware(async (req, re
   const { id } = req.params;
 
   if (!id || !mongoose.Types.ObjectId.isValid(id)) {
-    return errors.notFound(res, [ "test" ]);
+    return errors.notFound(res, [ "testSchema" ]);
   }
 
-  const test = await TestSchema
+  let testSchema = await TestSchema
     .findById(id)
     .populate("author", "label _id")
     .populate("subject", "code name")
     .populate("questions")
 
-  if (!test) {
-    return errors.notFound(res, [ "test" ])
+  if (!testSchema) {
+    return errors.notFound(res, [ "testSchema" ])
+  }
+  testSchema = testSchema.toObject();
+
+  // GETTING THE STATS OF THIS TEST SCHEMA
+
+  testSchema.questions.forEach(question => {
+    question.asked = 0; // how many times this question was used
+    question.answeredCorrectly = 0; // how many times this question was answered correctly
+  })
+
+  // all the tests which use this testSchema
+  let tests = await Test
+    .find({ testSchema: id })
+    .populate("course", "code");
+
+  tests = tests
+    .filter(test => test.status === "finished") // Filtering by virtual field; getting only finished tests
+    .map(test => test.toObject())
+
+  let totalGlobalScore = 0; // total score of all attempts
+  let totalAttempts = 0; // number of all attempts
+
+  for (test of tests) {
+    const attempts = await getTestAttempts({ test: test._id }, testSchema.questions, true, true); // all attempts in this test
+    let totalScore = 0; // total score of all attempts in this test
+    attempts.forEach(attempt => {
+      totalScore += attempt.score;
+      totalGlobalScore += attempt.score;
+      totalAttempts++;
+
+      attempt.questions.forEach(question => {
+        // Cheching how many times particular questions have been answered correctly
+        const questionIndex = testSchema.questions.findIndex(q => String(q._id) === String(question.questionSchema));
+        if (questionIndex > -1) {
+          testSchema.questions[questionIndex].asked++;
+          if (question.correct) {
+            testSchema.questions[questionIndex].answeredCorrectly++;
+          }
+        }
+      })
+    })
+
+    const averageScore = totalScore === 0 ? 0 : Math.floor(100 * totalScore / attempts.length) / 100; // average score in this test
+
+    test.averageScore = averageScore;    
   }
 
+  testSchema.tests = tests;
+  testSchema.totalAttempts = totalAttempts;
+  testSchema.averageScore = totalGlobalScore === 0 ? 0 : Math.floor(100 * totalGlobalScore / totalAttempts) / 100;
 
-  return res.send(test);
+
+  return res.send(testSchema);
 
 }));
 
